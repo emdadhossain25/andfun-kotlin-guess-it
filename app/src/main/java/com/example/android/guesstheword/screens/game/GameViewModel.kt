292e4c70a958fcb7c0f5ac44e75d56fd36085bff
emdadhossain25 <emdad@ergo-ventures.com>
2020-05-30 03:02:28 +0600

    added countdowntimer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,11 +1,27 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+
+    companion object {
+        const val DONE = 0L
+        const val ONE_SECOND = 1000L
+        const val COUNTDOWN_TIME = 10000L
+    }
+
+    //    timer field
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    private lateinit var timer: CountDownTimer
+
     // The current word
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
@@ -29,11 +45,26 @@ class GameViewModel : ViewModel() {
         nextWord()
         _score.value = 0
 
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntillFinished: Long) {
+                _currentTime.value = (millisUntillFinished / ONE_SECOND)
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventGameFinished.value = true
+            }
+
+        }
+        timer.start()
+
         Log.i("GameViemodel", "init block")
     }
 
     override fun onCleared() {
         super.onCleared()
+        timer.cancel()
         Log.i("GameViemodel", "oncleared block")
     }
 
@@ -43,11 +74,10 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-//            gameFinished()
-            _eventGameFinished.value = true
-        } else {
-            _word.value = wordList.removeAt(0)
+            resetList()
         }
+        _word.value = wordList.removeAt(0)
+
 
     }
 
